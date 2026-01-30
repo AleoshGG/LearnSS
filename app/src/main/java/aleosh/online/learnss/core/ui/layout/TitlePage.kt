71f958557589cce9174514041a31323b7025e9e5
Alexis Guzmán <spartanthealexvro@gmail.com>
2026-01-30 11:01:02 -0600

    [ADD]: Creando nueva vista, la leccion uno, con un layout global

---
+++ app/src/main/java/aleosh/online/learnss/core/ui/layout/TitlePage.kt
@@ -0,0 +1,23 @@
+package aleosh.online.learnss.core.ui.layout
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+
+@Composable
+fun TitlePageLayout(
+    title: String,
+) {
+    Text(
+        modifier = Modifier.fillMaxWidth(),
+        text = title.uppercase(),
+        style = MaterialTheme.typography.displaySmall,
+        color = MaterialTheme.colorScheme.primary,
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold
+    )
+}
---
+++ app/src/main/java/aleosh/online/learnss/features/lessons/solarSystem/ui/views/SolarSystemView.kt
@@ -0,0 +1,40 @@
+package aleosh.online.learnss.features.lessons.solarSystem.ui.views
+
+import aleosh.online.learnss.core.ui.layout.TitlePageLayout
+import aleosh.online.learnss.core.ui.theme.LearnSSTheme
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SolarSystemView() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(vertical = 32.dp)
+    ) {
+        TitlePageLayout(title = "solar system")
+    }
+
+}
+
+@Preview(
+    showSystemUi = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "Tablet Preview",
+    device = "spec:width=800dp,height=1280dp,dpi=240"
+)
+@Composable
+fun HomeViewPreview() {
+    LearnSSTheme {
+        SolarSystemView()
+    }
+}
